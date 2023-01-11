3cc8398933aa63600c8e196dd3265734d8e0bd5d
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-11 14:40:54 +0300

    add create a Post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
-import useStyles from "./styles";
+// import useStyles from "./styles";
 function Posts() {
   const posts = useSelector((state) => state.posts);
-  const classes = useStyles();
+  // const classes = useStyles();
 
   console.log(posts);
   return (
