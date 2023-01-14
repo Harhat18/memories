10b4767f875b04f6bacebc0cebc89e419b600f04
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-01-14 21:37:04 +0300

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import Navbar from "./components/Navbar/Navbar";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import { getPosts } from "./actions/posts";
-import useStyles from "./styles";
-import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
