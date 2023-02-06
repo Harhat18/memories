d2a2427356e249671a4baaa907497ae7ebd48567
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-02-06 22:10:37 +0300

    add

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { Pagination, PaginationItem } from '@material-ui/lab';
-import { Link } from 'react-router-dom';
-import { getPosts } from '../../actions/Posts';
-import { useSelector, useDispatch } from 'react-redux';
-import useStyles from './styles';
+import React, { useEffect } from "react";
+import { Pagination, PaginationItem } from "@material-ui/lab";
+import { Link } from "react-router-dom";
+import { getPosts } from "../../actions/Posts";
+import { useSelector, useDispatch } from "react-redux";
+import useStyles from "./styles";
 const Paginate = ({ page }) => {
   const dispatch = useDispatch();
   const { numberOfPages } = useSelector((state) => state.posts);
@@ -16,8 +16,8 @@ const Paginate = ({ page }) => {
       classes={{ ul: classes.ul }}
       count={numberOfPages}
       page={Number(page) || 1}
-      variant='outlined'
-      color='primary'
+      variant="outlined"
+      color="primary"
       renderItem={(item) => (
         <PaginationItem
           {...item}
